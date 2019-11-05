a0229afbbb7a123c971dc14fbcb9f665e83b76a3
Shah <s_shah107384@ats-hr420-dm23.fc.ca>
2019-11-05 14:49:13 -0500

    updated

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(() => {
+	console.log('fired!');
+
+	let player_lives = 5,
+		player = document.querySelector("#weapon"),
+		computer,
+		trigger = document.querySelector("#play"),
+		status = document.querySelector(".winlose"),
+		computer_lives = 5;
+
+		  
+	const choices = ["rock", "paper", "scissor"];
+
+	function playGame() {
+		console.log('clicked the button');
+
+		computer = choices[Math.floor(Math.random() * choices.length)];
+
+		let player_choice = player.value;
+		console.log("computer:", computer, "player:", player_choice);
+
+		// this is a comment. check for a tie
+		if(computer == player_choice) {
+			// tie - no one wins
+			status.textContent = "Tie! you live to shoot another day!";
+		} else {
+			status.textContent = "";
+		}
+
+	}
+
+	trigger.addEventListener("click", playGame);
+})();
